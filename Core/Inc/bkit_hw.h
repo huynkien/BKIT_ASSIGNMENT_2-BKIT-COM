560cff20c0a2ec835ba9be83ffd9538d5d3eed13
huynkien <trungkienh524@gmail.com>
2026-01-26 00:28:02 +0700

    feat: comment and adding condition to check overflow of ring buffer

--- Core/Inc/bkit_hw.h
+++ Core/Inc/bkit_hw.h
@@ -10,17 +10,39 @@
 
 #include "main.h"
 
+/* BKIT-COM status definitions:
+ * BKIT_COM_OK:
+ *  - Init:    UART peripheral initialized and Interrupt enabled successfully.
+ *  - Send:    Transmission completed successfully 
+ *  - Receive: Buffer processing finished 
+ *
+ * BKIT_COM_ERROR:
+ *  - Init:    Failed to enable UART/Interrupt (HAL_UART_Receive_IT returned error).
+ *
+ * BKIT_COM_BUSY:
+ *  - Send:    UART hardware is currently transmitting another frame.
+ */
+
 typedef enum {
 	BKIT_COM_OK,
 	BKIT_COM_ERROR,	
-	BKIT_COM_BUSY
+	BKIT_COM_BUSY,
+	BKIT_COM_RX_OVERFLOW
 } bkit_status_t;
 
-// Struct for protocol hardware interface layer
+
+
+
+/* @brief Struct for protocol hardware interface layer
+ * - hw_init: Initialize the peripheral (UART, ...).
+ * - hw_send: Transmit a sequence of bytes.
+ * - hw_receive: Read available data from the internal driver buffer (Ring Buffer) into the user application buffer.
+ */
+
 typedef struct {
 	bkit_status_t (*hw_init)(void);
-	bkit_status_t (*hw_send_byte)(uint8_t* data, uint16_t length);
-	bkit_status_t (*hw_receive_byte)(uint8_t* buffer, uint16_t length, uint16_t *received_msg_length);
+	bkit_status_t (*hw_send)(uint8_t* msg, uint16_t length);
+	bkit_status_t (*hw_receive)(uint8_t* buffer, uint16_t length, uint16_t *received_msg_length);
 } bkit_interface_t;
 
 extern bkit_interface_t bkit_uart_driver;
--- Core/Src/bkit_hw.c
+++ Core/Src/bkit_hw.c
@@ -14,17 +14,34 @@ static uint8_t  rx_buffer[RX_BUFFER_SIZE] = {0};
 static uint16_t volatile head_ptr = 0;				// pointer to write message to buffer
 static uint16_t volatile tail_ptr = 0;				// pointer to read message from buffer
 static uint8_t  rx_temp_byte = 0;					// read temporary received byte
+static uint8_t  volatile rx_overflow_flag = 0;
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
-	if (huart->Instance == USART6) {
-		rx_buffer[head_ptr] = rx_temp_byte;
-		head_ptr++;
-		if (head_ptr >= RX_BUFFER_SIZE) {
-			head_ptr = 0;
-		}
+	if (huart->Instance == USART6) { // read byte when have signal
+        uint16_t next_head = head_ptr + 1;
+        if (next_head >= RX_BUFFER_SIZE) {
+            next_head = 0;
+        }
+		// Check if buffer is full or not
+        if (next_head != tail_ptr) { //buffer is not full
+            rx_buffer[head_ptr] = rx_temp_byte; 
+            head_ptr = next_head;
+			rx_overflow_flag = 0;
+        } 
+        else { //buffer is full
+			rx_overflow_flag = 1; //There are not specific case to process this warning :>
+        }
 		HAL_UART_Receive_IT(&huart6, &rx_temp_byte, 1);
 	}
 }
+
+
+
+/**
+  * @brief  Initialize the peripheral (UART, ...).
+  * @param  None
+  * @retval BKIT-COM status.
+  */
 
 bkit_status_t hw_init_uart() {
 	if (HAL_UART_Receive_IT(&huart6, &rx_temp_byte, 1) != HAL_OK) {
@@ -33,6 +50,13 @@ bkit_status_t hw_init_uart() {
 	return BKIT_COM_OK;
 }
 
+/**
+  * @brief  Transmit a sequence of bytes.
+  * @param  msg Pointer to the data buffer to send.
+  * @param  length Number of bytes to send.
+  * @retval BKIT-COM status.
+  */
+
 bkit_status_t hw_send_uart(uint8_t *msg, uint16_t length) {
 	if (HAL_UART_Transmit(&huart6, msg, length, 256) != HAL_OK) {
 		return BKIT_COM_BUSY;
@@ -40,22 +64,31 @@ bkit_status_t hw_send_uart(uint8_t *msg, uint16_t length) {
 	return BKIT_COM_OK;
 }
 
-bkit_status_t hw_receive_uart(uint8_t *buffer, uint16_t max_length, uint16_t received_msg_length) {
+/**
+  * @brief  Read available data from the internal driver buffer (Ring Buffer)
+  * 		into the user application buffer.
+  * @param  buffer Pointer to the user's destination buffer.
+  * @param  max_length Maximum capacity of the user's buffer.
+  * @param  received_msg_length Output pointer, the driver writes
+  * 							actual number of bytes read into this variable.
+  * @retval BKIT-COM status.
+  */
+
+bkit_status_t hw_receive_uart(uint8_t *buffer, uint16_t max_length, uint16_t *received_msg_length) {
 	uint16_t count = 0;
+	*received_msg_length = 0;
 	while (tail_ptr != head_ptr && count < max_length) {
-		buffer[count] = rx_buffer[tail_ptr];
-		tail_ptr++;
-		count++;
+		buffer[count++] = rx_buffer[tail_ptr++];
 		if (tail_ptr >= RX_BUFFER_SIZE) {
 			tail_ptr = 0;
 		}
-		*received_msg_length = count;
 	}
+	*received_msg_length = count;
 	return BKIT_COM_OK;
 }
 
 bkit_interface_t bkit_uart_driver = {
 	.hw_init = hw_init_uart,
-	.hw_send_byte = hw_send_uart,
-	.hw_receive_byte = hw_receive_uart
+	.hw_send = hw_send_uart,
+	.hw_receive = hw_receive_uart
 };
